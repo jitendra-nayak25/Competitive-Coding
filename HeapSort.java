ed27f9a9b84530bda93686aac207ff51f02abb18
LENOVO <mrnxtdeveloper@gmail.com>
2026-08-29 23:28:30 +0530

    Initial commit

---
+++ HeapSort.java
@@ -0,0 +1,58 @@
+public class HeapSort {
+
+    static void heapSort(int[] arr) {
+
+        int n = arr.length;
+
+      
+        for (int i = n / 2 - 1; i >= 0; i--) {
+            heapify(arr, n, i);
+        }
+
+        for (int i = n - 1; i > 0; i--) {
+
+            int temp = arr[0];
+            arr[0] = arr[i];
+            arr[i] = temp;
+
+            heapify(arr, i, 0);
+        }
+    }
+
+    static void heapify(int[] arr, int n, int i) {
+
+        int largest = i;
+        int left = 2 * i + 1;
+        int right = 2 * i + 2;
+
+        if (left < n && arr[left] > arr[largest]) {
+            largest = left;
+        }
+
+        if (right < n && arr[right] > arr[largest]) {
+            largest = right;
+        }
+
+        if (largest != i) {
+
+            int temp = arr[i];
+            arr[i] = arr[largest];
+            arr[largest] = temp;
+
+            heapify(arr, n, largest);
+        }
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr = {50, 20, 70, 10, 40, 30, 60};
+
+        heapSort(arr);
+
+        System.out.println("Sorted Array:");
+
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+    }
+}
